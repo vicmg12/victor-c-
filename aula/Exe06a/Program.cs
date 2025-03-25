8b5385d331aa517e137eecc6c021232d7854af6a
VICTOR MOURA GASPARETTO <victor.gasparetto@cs.up.edu.br>
2025-03-25 09:25:04 -0300

    novo

---
+++ aula/Exe06a/Program.cs
@@ -0,0 +1,33 @@
+﻿//1 - Criar um vetor de 100 posições inteiras
+//2 - Criar um loop para percorrer todo o vetor
+//3 - Preencher cada posição com um valor aleatório
+//4 - Imprimir o vetor na tela
+
+int[] vetor = new int[100];
+Random r = new Random();
+
+for(int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(100);
+}
+
+for(int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+for(int i = 0; i < vetor.Length - 1; i++)
+{
+    if(vetor[i] > vetor[i + 1])
+    {
+        int aux = vetor[i];
+        vetor[i] = vetor[i + 1];
+        vetor[i + 1] = aux;
+    }
+}
+
+Console.WriteLine("\n");
+for(int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
